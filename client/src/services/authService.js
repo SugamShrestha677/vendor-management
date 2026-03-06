0df09d69a4ce5a1faf60764dcf65c1cf248083e0
Sugam Shrestha <info@shresthasugam.com.np>
2026-03-07 01:39:16 +0545

    authService.js file modified

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -4,6 +4,11 @@ const API_URL = 'http://localhost:5000/api/auth'
 
 export const login = async (email, password) => {
   const response = await axios.post(`${API_URL}/login`, { email, password })
+
+  if (response.data.token) {
+    localStorage.setItem("token", response.data.token)
+  }
+
   return response.data
 }
 
