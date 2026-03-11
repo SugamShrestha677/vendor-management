d9989e608aac9ba9ab7c4db63bb1740575e07175
Sugam Shrestha <info@shresthasugam.com.np>
2026-03-11 07:52:31 +0545

    frontend url updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://vendor-management-backend-yte6.onrender.com',
+        target: process.env.VITE_API_URL || 'http://localhost:5000',
         changeOrigin: true
       }
     }
